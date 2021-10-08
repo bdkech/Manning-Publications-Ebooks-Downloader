6ec9d6a17dd4d02a1ddf2baee10253e5f2ad1f26
bobak <bdkech@gmail.com>
2021-10-08 14:09:10 -0400

    changed to selenium method

--- manning.py
+++ manning.py
@@ -1,25 +1,30 @@
 #!venv/bin/python3
 
 """
-Date:    Wed May 12 02:56:46 UTC 2021
-Author:  Lucian Maly <lucian@redhat.com>
+Date:    Thu 07 Oct 2021 11:39:45 PM EDT
+Author:  Bobak Kechavarzi <bdkech@gmail.com>
 License: MIT
 """
 
-import requests
 from bs4 import BeautifulSoup
+import time
 import datetime
 import os
 import errno
 import sys
 import getopt
 
-loginURL = 'https://login.manning.com/login?service=https://www.manning.com/login/cas'
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 
 # Defaults
 limit = 5
 sortby = 'lastUpdated'
-
+options = Options()
+options.headless = True
 
 def main(argv):
     global username
@@ -75,79 +80,59 @@ def create_folder():
 
 
 def get_list():
-    dashboardURL = 'https://www.manning.com/dashboard/index?filter=book&max=' + \
-        str(limit)+'&order='+sortby+'&sort=desc'
-    with requests.Session() as s:
-        # PURPOSE: Get the value of LT
-        # soup0 = BeautifulSoup(s.get(loginURL).text, 'html.parser')
-        headers = {
-            'Origin': 'https://login.manning.com/',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-        }
-        data = {
-            'username': username,
-            'password': password,
-            # 'lt': soup0.find('input', {'name': 'lt'}).get('value'),
-            'execution': 'e1s1',
-            '_eventId': 'submit',
-            'geolocation': '',
-            'submit': ''
-        }
-        # PURPOSE: Log in
-        s.post('https://login.manning.com/login', cookies=s.cookies, headers=headers, data=data)
-        dashboard = s.get(dashboardURL)
+# If you want to open Firefox
+        driver = webdriver.Firefox(options=options, service_log_path='/dev/null')
+
+        driver.get('https://login.manning.com/login?service=https%3A%2F%2Fwww.manning.com%2Flogin%2Fcas')
+        username_ele = driver.find_element_by_id("username-sign-in")
+        password_ele = driver.find_element_by_id("password-sign-in")
+        username_ele.send_keys(username)
+        password_ele.send_keys(password)
+        driver.find_element_by_name("submit").click()
+        time.sleep(4)
+        driver.get('https://www.manning.com/dashboard/index?filter=all&order=purchaseDate&sort=asc')
+        page_source = driver.page_source
         # PURPOSE: Parse the dashboard with up to 999 products
-        soup = BeautifulSoup(dashboard.text, 'html.parser')
+        soup = BeautifulSoup(page_source, 'html.parser')
         div_container = soup.find('table', {'id': 'productTable'})
         for product in div_container.find_all('tr', {'class': 'license-row'}):
+            # EXAMPLE: Terraform in Action
+            title = str(product.find(
+                'div', {'class': 'product-title'}).text.strip())
+            print(title)
+            # PURPOSE: Find all the restrictedDownloadIds and create a complete payload
+            for downloadSelection in product.find_all('div', {'class': 'download-selection'}):
+                hidden = downloadSelection.find_all(
+                    'input', {'type': 'hidden'})
+                for val in hidden:
+                    checkbox1 = (val['id'], val['value'])
+                    checkbox2 = (restrictedDownloadIds, val['id'])
+                    download_payload.append(checkbox1)
+                    download_payload.append(checkbox2)
+                    # EXAMPLE: [('dropbox', 'false'), ('productExternalId', 'winkler'), ('1971', '7850702'), ('winkler-restrictedDownloadIds', '1971'), ('1972', '7850703'), ('winkler-restrictedDownloadIds', '1972'), ('1973', '7850704'), ('winkler-restrictedDownloadIds', '1973')]
             try:
-                # EXAMPLE: Terraform in Action
-                title = str(product.find(
-                    'div', {'class': 'product-title'}).text.strip())
-                # EXAMPLE: winkler
-                author = str(product.find(
-                    'form', {'class': 'download-form'})['name']).split('-')[1]
-                restrictedDownloadIds = author + '-restrictedDownloadIds'
-                download_payload = [
-                    ('dropbox', 'false'),
-                    ('productExternalId', author)
-                ]
-                # PURPOSE: Find all the restrictedDownloadIds and create a complete payload
-                for downloadSelection in product.find_all('div', {'class': 'download-selection'}):
-                    hidden = downloadSelection.find_all(
-                        'input', {'type': 'hidden'})
-                    for val in hidden:
-                        checkbox1 = (val['id'], val['value'])
-                        checkbox2 = (restrictedDownloadIds, val['id'])
-                        download_payload.append(checkbox1)
-                        download_payload.append(checkbox2)
-                        # EXAMPLE: [('dropbox', 'false'), ('productExternalId', 'winkler'), ('1971', '7850702'), ('winkler-restrictedDownloadIds', '1971'), ('1972', '7850703'), ('winkler-restrictedDownloadIds', '1972'), ('1973', '7850704'), ('winkler-restrictedDownloadIds', '1973')]
-                try:
-                    subfolder = str(title.replace(' ', '_'))
-                    path = os.path.join(folder, subfolder)
-                    os.makedirs(path)
-                    print('Created folder', path)
-                except OSError as e:
-                    if e.errno == errno.EEXIST:
-                        print(f'Directory {path} already exists.')
-                    else:
-                        raise
-                downloadURL = 'https://www.manning.com/dashboard/download?id=downloadForm-' + author
-                print('Downloading', title, '...')
-                dl = s.post(downloadURL, cookies=s.cookies,
-                            headers=headers, data=download_payload)
-                # PURPOSE: Some free titles are only in PDF format, this can be determined from the amount of hidden inputs
-                if len(download_payload) <= 4:
-                    extension = '.pdf'
+                subfolder = str(title.replace(' ', '_'))
+                path = os.path.join(folder, subfolder)
+                os.makedirs(path)
+                print('Created folder', path)
+            except OSError as e:
+                if e.errno == errno.EEXIST:
+                    print(f'Directory {path} already exists.')
                 else:
-                    extension = '.zip'
-                filename = path + '/' + subfolder + extension
-                file = open(filename, "wb")
-                file.write(dl.content)
-                file.close()
-            except TypeError:
-                pass
-
+                    raise
+            downloadURL = 'https://www.manning.com/dashboard/download?id=downloadForm-' + author
+            print('Downloading', title, '...')
+            dl = s.post(downloadURL, cookies=s.cookies,
+                        headers=headers, data=download_payload)
+            # PURPOSE: Some free titles are only in PDF format, this can be determined from the amount of hidden inputs
+            if len(download_payload) <= 4:
+                extension = '.pdf'
+            else:
+                extension = '.zip'
+            filename = path + '/' + subfolder + extension
+            file = open(filename, "wb")
+            file.write(dl.content)
+            file.close()
 
 def end_script():
     print('Exiting the script...')
