7feef726f5a0dd2fbb261938b92ce2d1f23093b3
bobak <bdkech@gmail.com>
2021-10-10 23:45:21 -0400

    playing with profile settings

--- manning.py
+++ manning.py
@@ -13,6 +13,7 @@ import os
 import errno
 import sys
 import getopt
+import re
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -24,7 +25,14 @@ from selenium.webdriver.firefox.options import Options
 limit = 5
 sortby = 'lastUpdated'
 options = Options()
-options.headless = True
+options.headless = False
+fp=webdriver.FirefoxProfile()
+fp.set_preference("browser.helperApps.neverAsk.openFile", 'application/pdf')
+fp.set_preference("browser.helperApps.neverAsk.saveToDisk", 'application/pdf')
+fp.set_preference("browser.download.panel.shown", False)
+fp.set_preference("browser.download.folderList",2)
+fp.set_preference("browser.download.manager.showWhenStarting",False)
+fp.set_preference("browser.download.dir", "~/Downloads/")
 
 def main(argv):
     global username
@@ -80,8 +88,9 @@ def create_folder():
 
 
 def get_list():
+        base_url = 'https://www.manning.com'
 # If you want to open Firefox
-        driver = webdriver.Firefox(options=options, service_log_path='/dev/null')
+        driver = webdriver.Firefox(firefox_profile=fp, options=options, service_log_path='/dev/null')
 
         driver.get('https://login.manning.com/login?service=https%3A%2F%2Fwww.manning.com%2Flogin%2Fcas')
         username_ele = driver.find_element_by_id("username-sign-in")
@@ -89,61 +98,18 @@ def get_list():
         username_ele.send_keys(username)
         password_ele.send_keys(password)
         driver.find_element_by_name("submit").click()
-        time.sleep(4)
+        time.sleep(5)
         driver.get('https://www.manning.com/dashboard/index?filter=all&order=purchaseDate&sort=asc')
-        page_source = driver.page_source
-        print(page_source)
-        # PURPOSE: Parse the dashboard with up to 999 products
-        soup = BeautifulSoup(page_source, 'html.parser')
-        div_container = soup.find('table', {'id': 'productTable'})
-        for product in div_container.find_all('tr', {'class': 'license-row'}):
-            # EXAMPLE: Terraform in Action
-            title = str(product.find(
-                'div', {'class': 'product-title'}).text.strip())
-            # PURPOSE: Find all the restrictedDownloadIds and create a complete payload
-            for downloadSelection in product.find_all('div', {'class': 'download-selection'}):
-                hidden = downloadSelection.find_all(
-                    'input', {'type': 'hidden'})
-                for val in hidden:
-                    checkbox1 = (val['id'], val['value'])
-                    checkbox2 = (restrictedDownloadIds, val['id'])
-                    download_payload.append(checkbox1)
-                    download_payload.append(checkbox2)
-                    # EXAMPLE: [('dropbox', 'false'), ('productExternalId', 'winkler'), ('1971', '7850702'), ('winkler-restrictedDownloadIds', '1971'), ('1972', '7850703'), ('winkler-restrictedDownloadIds', '1972'), ('1973', '7850704'), ('winkler-restrictedDownloadIds', '1973')]
-            try:
-                subfolder = str(title.replace(' ', '_'))
-                path = os.path.join(folder, subfolder)
-                os.makedirs(path)
-                print('Created folder', path)
-            except OSError as e:
-                if e.errno == errno.EEXIST:
-                    print(f'Directory {path} already exists.')
-                else:
-                    raise
-            downloadURL = 'https://www.manning.com/dashboard/download?id=downloadForm-' + author
-            print('Downloading', title, '...')
-            dl = s.post(downloadURL, cookies=s.cookies,
-                        headers=headers, data=download_payload)
-            # PURPOSE: Some free titles are only in PDF format, this can be determined from the amount of hidden inputs
-            if len(download_payload) <= 4:
-                extension = '.pdf'
-            else:
-                extension = '.zip'
-            filename = path + '/' + subfolder + extension
-            file = open(filename, "wb")
-            file.write(dl.content)
-            file.close()
-
-def end_script():
-    print('Exiting the script...')
-    sys.exit()
-
+        time.sleep(5)
+        dl_links = driver.find_elements_by_css_selector("[href*='PDF']")
+        for dl_link in dl_links:
+            print(dl_link.get_attribute('href'))
+        driver.get(dl_link.get_attribute('href'))
 
 if len(sys.argv) > 1:
     main(sys.argv[1:])
-    # create_folder()
+    create_folder()
     get_list()
-    end_script()
 else:
     print('Help: `manning.py -h`')
     sys.exit()
